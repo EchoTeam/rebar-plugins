0608adf342d782586d18c4689b0dc93e21a6a69c
snaky <snaky@aboutecho.com>
2013-11-27 11:45:00 +0400

    Make it possible to process several appup.src files

--- src/rebar_genappup_plugin.erl
+++ src/rebar_genappup_plugin.erl
@@ -5,23 +5,30 @@
     'post_compile'/2
 ]).
 
-'post_compile'(_Config, _AppFile) ->
-    AppDir = rebar_utils:get_cwd(),
-    AppUpSrc = appup_src_name(AppDir),
-    AppUp = appup_name(AppDir),
-    case file:read_file_info(AppUpSrc) of
-        {error, enoent} -> ok;
-        {ok, _} ->
+'post_compile'(Config, _AppFile) ->
+    SrcDir  = filename:join(rebar_utils:get_cwd(), "src"),
+    Files = filelib:wildcard(SrcDir ++ "/*.appup.src"),
+    lists:foreach(fun(File) ->
+        copy_appup_src(Config, File)
+    end, Files).
+
+copy_appup_src(Config, Src) ->
+    App  = filename:basename(Src, ".appup.src"),
+    Dest = filename:join([get_deps_dir(Config, App),
+        "ebin", App ++ ".appup"]),
+    DestDir = filename:dirname(Dest),
+    case filelib:is_dir(DestDir) of
+        true ->
             io:format("Found ~s. Making ~s...~n",
-                [filename:basename(AppUpSrc), filename:basename(AppUp)]),
-            {ok, _} = file:copy(AppUpSrc, AppUp),
-            ok
+                [filename:basename(Src), filename:basename(Dest)]),
+            {ok, _} = file:copy(Src, Dest);
+        false ->
+            io:format("ERROR: Can't process ~p. Path ~p not found.~n",
+                [Src, DestDir]),
+            halt(1)
     end.
 
-appup_src_name(AppDir) ->
-    AppName = filename:basename(AppDir),
-    filename:join([AppDir, "src", AppName ++ ".appup.src"]).
-
-appup_name(AppDir) ->
-    AppName = filename:basename(AppDir),
-    filename:join([AppDir, "ebin", AppName ++ ".appup"]).
+get_deps_dir(Config, App) ->
+    BaseDir = rebar_config:get_xconf(Config, base_dir, []),
+    DepsDir = rebar_config:get_xconf(Config, deps_dir, "deps"),
+    filename:join([BaseDir, DepsDir, App]).
